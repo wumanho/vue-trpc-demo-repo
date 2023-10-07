b2f350a240e0375aeefa94ea3383ba3374532c55
huwenhao <wumanho@tutanota.com>
2023-10-07 13:50:14 +0800

    feat：添加请求 headers

--- packages/client/src/hooks/useTRPC.ts
+++ packages/client/src/hooks/useTRPC.ts
@@ -3,9 +3,10 @@ import { AnyRouter } from '@trpc/server'
 
 export function useTRPC<Router extends AnyRouter>(
   url: string,
+  headers?: Parameters<typeof httpLink>[0]['headers'],
   transformer?: Parameters<typeof createTRPCProxyClient>[0]['transformer']
 ) {
-  const httpLinkConfig = httpLink({ url: url })
+  const httpLinkConfig = httpLink({ url: url, headers })
   const trpc = createTRPCProxyClient<Router>({
     transformer,
     links: [httpLinkConfig]
